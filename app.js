f989a32d5bdbc92512ecb5393ae8cc5a6f55ef7f
Savchenko Maxim <savchenko@mos-team.ru>
2022-06-12 17:08:00 +0300

    Add actions and fix something

--- app.js
+++ app.js
@@ -2,14 +2,30 @@ let express = require('express')
 let http = require('http')
 let nunjucks = require('nunjucks')
 let path = require('path')
+let livereload = require('livereload')
+let connectLivereload = require('connect-livereload')
 let sassMiddleware = require('node-sass-middleware')
+let bodyParser = require('body-parser')
 
 let app = express()
+
+let liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "views"));
+
+liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+      liveReloadServer.refresh("/");
+    }, 50);
+  });
 
 nunjucks.configure('views', {
     autoescape: true,
     express: app
 })
+
+app.use(connectLivereload());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(sassMiddleware({
     src: path.join(__dirname,  'bootstrap'),
@@ -23,15 +39,19 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', function(req,res,next) {
     let data = {
         content: 'Hello World!', 
-        title: 'Example'
+        title: 'MCH'
     }
 
     res.render('index.njk', data)
 })
 
+app.post('/send', function(req, res) {
+    console.log(req.body)
+})
+
 let server = http.createServer(app)
 
-server.listen('3042', () => {
-    console.log('Listening on port 3042')
+server.listen('3046', () => {
+    console.log('Listening on port 3046')
   })
 
